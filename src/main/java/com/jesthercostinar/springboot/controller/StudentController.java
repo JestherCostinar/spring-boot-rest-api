3bbbac05819983e24be65b0aeb2fe249b1bbcbc8
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-04 13:53:01 +0800

    Get Mapping return list in JSON format

--- src/main/java/com/jesthercostinar/springboot/controller/StudentController.java
+++ src/main/java/com/jesthercostinar/springboot/controller/StudentController.java
@@ -8,14 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
-    // Spring Boot REST API using GetMapping
+    // Spring Boot REST API using GetMapping that return JSON
     @GetMapping("student")
     public ResponseEntity<Student> getStudent() {
         Student student = new Student(1, "Jesther", "Costinar");
         return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    // Spring REST API that return a list in JSON format
+    @GetMapping("students")
+    public ResponseEntity<List<Student>> getStudents() {
+        List <Student> students = new ArrayList<>();
+        students.add(new Student(1, "Jesther", "Costinar"));
+        students.add(new Student(2, "Jersey", "Alto"));
+        students.add(new Student(3, "Franken", "Alto"));
+        return ResponseEntity.ok(students);
+
     }
 }
